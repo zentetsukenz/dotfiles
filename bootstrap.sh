7129f9fa886f8ed6dcd6293ffedf70f07ab6d1ed
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-05-05 23:47:29 +0700

    Install powerline fonts
    Wanna use Hack font. It's look easier to read.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,12 +13,28 @@ function doIt() {
     -avh --no-perms . ~;
 }
 
+function install_powerline_fonts() {
+  if [ -d "fonts" ]; then
+    rm -rf fonts
+  fi
+
+  # clone
+  git clone https://github.com/powerline/fonts.git
+  # install
+  cd fonts
+  ./install.sh
+  # clean-up a bit
+  cd ..
+  rm -rf fonts
+}
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt;
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
+    install_powerline_fonts;
     doIt;
   fi;
 fi;
