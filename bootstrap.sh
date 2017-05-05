ad68f530c5ed62a801e284cda25185dcd705b308
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-05-06 01:07:28 +0700

    Add step annoucement
    So we know where are we.

--- bootstrap.sh
+++ bootstrap.sh
@@ -46,7 +46,10 @@ else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Installing Powerline fonts"
     install_powerline_fonts;
+
+    echo "Syncing the file to ~/ folder"
     doIt;
   fi;
 fi;
