d333a2f6874c242c4d9f29e2aab99ae67a86de88
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-05-07 14:18:08 +0700

    Install cmake to compile vim YCM
    YCM need to manually compile it ycm_core which I don't know how to do it
    yet.

--- brew.sh
+++ brew.sh
@@ -66,6 +66,7 @@ brew install webkit2png
 brew install zopfli
 brew install ctags
 brew install reattach-to-user-namespace
+brew install cmake
 
 # Remove outdated versions from the cellar.
 brew cleanup
