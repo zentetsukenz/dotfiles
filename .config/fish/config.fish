a04b021806e103e8751bffb51b935d05d560c7df
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-05-10 23:51:02 +0700

    Store core/find utils prefix path
    To speed up startup time a little bit.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,19 @@
+if not set -q __fish_prefix_coreutils
+    set -g __fish_prefix_coreutils (brew --prefix coreutils)
+end
+
+if not set -q __fish_prefix_findutils
+    set -g __fish_prefix_findutils (brew --prefix findutils)
+end
+
 set -x -g LS_COLORS "di=38;5;27:fi=38;5;7:ln=38;5;51:pi=40;38;5;11:so=38;5;13:or=38;5;197:mi=38;5;161:ex=38;5;9:"
 
 set -x -g TERM "xterm-256color"
 
 # Coreutils bin and man folders
-set -x -g PATH (brew --prefix coreutils)/libexec/gnubin $PATH
-set -x -g MANPATH (brew --prefix coreutils)/libexec/gnuman $MANPATH
+set -x -g PATH $__fish_prefix_coreutils/libexec/gnubin $PATH
+set -x -g MANPATH $__fish_prefix_coreutils/libexec/gnuman $MANPATH
 
 # Findutils bin and man folders
-set -x -g PATH (brew --prefix findutils)/libexec/gnubin $PATH
-set -x -g MANPATH (brew --prefix findutils)/libexec/gnuman $MANPATH
+set -x -g PATH $__fish_prefix_findutils/libexec/gnubin $PATH
+set -x -g MANPATH $__fish_prefix_findutils/libexec/gnuman $MANPATH
