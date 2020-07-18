5fd61c4cfd86a6371b9727e8767a83e1aaa7ea8f
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2020-07-18 21:18:09 +0700

    Use Erlang 23

--- fish_config.fish
+++ fish_config.fish
@@ -46,7 +46,7 @@ status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
 
 # Erlang
-status --is-interactive; and . "$HOME/kerl/22.2/activate.fish"
+status --is-interactive; and . "$HOME/kerl/23.0/activate.fish"
 
 # Elixir
 status --is-interactive; and source "$HOME/.kiex/scripts/kiex.fish"
