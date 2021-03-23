42bb7c77caf03e9143eacae10d001c911ca6e48c
Wiwatta Mongkhonchit <cheab@opn.ooo>
2021-03-23 15:19:35 +0700

    Let's remove krew for now

--- brew.sh
+++ brew.sh
@@ -62,17 +62,5 @@ brew install tree
 brew install reattach-to-user-namespace
 brew install cmake
 
-# Install kubectl krew plugin
-begin
-  set -x; set temp_dir (mktemp -d); cd "$temp_dir" &&
-  set OS (uname | tr '[:upper:]' '[:lower:]') &&
-  set ARCH (uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/') &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
-  tar zxvf krew.tar.gz &&
-  set KREWNAME krew-$OS"_"$ARCH &&
-  ./$KREWNAME install krew &&
-  set -e KREWNAME; set -e temp_dir
-end
-
 # Remove outdated versions from the cellar.
 brew cleanup
